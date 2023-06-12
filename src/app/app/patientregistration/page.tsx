c23d8c5182b3a2acec6fb1ed2ca9f2042eb214ad
Everton Fernandes <evertonfernandes.dev@gmail.com>
2023-06-11 22:59:24 -0300

    feat: :sparkles: layout adjustments

--- src/app/app/patientregistration/page.tsx
+++ src/app/app/patientregistration/page.tsx
@@ -8,7 +8,7 @@ export default function PatientRegistration() {
           <input
             type="text"
             id="floating_outlined"
-            className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg  border border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
             placeholder=" "
           />
           <label
