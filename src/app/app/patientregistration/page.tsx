80f53717b22ca6620ffb273ef80c6eb07662a1e0
Everton Fernandes <evertonfernandes.jsx@gmail.com>
2023-06-15 00:09:24 -0300

    feat: :sparkles: added table grid mui

--- src/app/app/patientregistration/page.tsx
+++ src/app/app/patientregistration/page.tsx
@@ -2,24 +2,39 @@
 
 import { Breadcump } from '../components/breadcump'
 import { Card } from '../components/card'
-import { DataGrid, GridColDef, GridRowsProp } from '@mui/x-data-grid'
+import { DataGrid, GridColDef, ptBR } from '@mui/x-data-grid'
 
-export default function PatientRegistration() {
-  const rows: GridRowsProp = [
-    { id: 1, col1: 'Hello', col2: 'World' },
-    { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
-    { id: 3, col1: 'MUI', col2: 'is Amazing' },
-  ]
-
+export default async function PatientRegistration() {
   const columns: GridColDef[] = [
-    { field: 'col1', headerName: 'Column 1', width: 150 },
-    { field: 'col2', headerName: 'Column 2', width: 150 },
+    { field: 'patient_name', headerName: 'Column 1', width: 150 },
+    { field: 'patient_cpf', headerName: 'Column 2', width: 150 },
+    { field: 'patient_age', headerName: 'Column 2', width: 150 },
+    { field: 'patient_gender', headerName: 'Column 2', width: 150 },
+    { field: 'responsible_name', headerName: 'Column 2', width: 150 },
+    { field: 'responsible_cpf', headerName: 'Column 2', width: 150 },
+    { field: 'responsible_age', headerName: 'Column 2', width: 150 },
+    { field: 'responsible_gender', headerName: 'Column 2', width: 150 },
+    { field: 'Degree_kinship_responsible', headerName: 'Column 2', width: 150 },
+    { field: 'responsible_phone', headerName: 'Column 2', width: 150 },
   ]
+
   return (
     <div className="space-y-6">
       <Breadcump descriptionCurrentPage="Cadastro de Pacientes" />
       <Card title="Lista de pacientes">
-        <DataGrid rows={rows} columns={columns} />
+        <DataGrid
+          rows={[]}
+          columns={columns}
+          getRowId={(row) => row.entidade_id}
+          checkboxSelection
+          autoHeight
+          rowHeight={60}
+          initialState={{
+            pagination: { paginationModel: { pageSize: 10 } },
+          }}
+          localeText={ptBR.components.MuiDataGrid.defaultProps.localeText}
+          pageSizeOptions={[10, 25, 50]}
+        />
       </Card>
     </div>
   )
